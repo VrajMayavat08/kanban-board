67bbc6e68dbd8603946192413fdf61ed85d709a4
Vraj Mayavat <vrajmayavat08@gmail.com>
2026-07-05 14:30:23 -0400

    added models for the actual Board, Card and List

---
+++ kanban-server/models/Card.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, default: '' },
+  list: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: true },
+  board: { type: mongoose.Schema.Types.ObjectId, ref: 'Board', required: true },
+  position: { type: Number, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Card', cardSchema);
